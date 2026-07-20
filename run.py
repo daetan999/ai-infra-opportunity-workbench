36c9a86ef2438edcfbe8a472b23e73658d13a365
daetan999 <daetan999@gmail.com>
2026-07-20 17:12:07 +0800

    Use production-compatible application runner

--- run.py
+++ run.py
@@ -1,9 +1,4 @@
-import app as myapp
-
-print("✅ Imported app from:", myapp.__file__)
-print("✅ Routes in this app:")
-for r in myapp.app.routes:
-    print(" -", getattr(r, "path", r))
-
 import uvicorn
-uvicorn.run(myapp.app, host="127.0.0.1", port=8000, log_level="debug")
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=False)
